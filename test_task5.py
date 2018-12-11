ea2956e00f0097d100adcaac7ca0178fe5d570e6
TayfunKaraderi <karaderitayfun@gmail.com>
2018-12-11 15:54:17 +0000

    bug fixes to the test_task5

--- test_task5.py
+++ test_task5.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -
+import numpy as np
 xx=[-1, 0 ,0.5, 1, 3, 0, 2, 0, 1, 2, 0, 2, 2, 1, 2, -2]
 yy=np.array([20, 1, 1, 2, 6, 7, 8, 3, 3, 0, 4, 3, 4, 4, 5, 20])
 
@@ -16,18 +17,17 @@ for i in range(len(boundary)):
         y.append(yy[i]);
         t.append(tt[i])
 
-
 # time values without repetition
 ts= list(set(t))
 tindex = t_index(t)
-
-Crest_xcoords, Crest_height, ts, tslosh = peak(x, y, t, tindex, ts, 1, 2)
 
 
 def test_task5():
     #Output_analytic = ode_solve.xx(inputs)
     #output_numeric =
-        assert  (Crest_xcoords == [1, 2, 0, 0, 2])
-        assert  (Crest_height == [2.0, 8.0, 3.0, 4.0, 5.0])
-        assert  (ts == [0, 1, 2, 3, 4])
-        assert  (tslosh == 1)
+    Crest_xcoords, Crest_height, ts, tslosh = peak(x, y, t, tindex, ts, 1, 2)
+
+    assert  (Crest_xcoords == [1, 2, 0, 0, 2])
+    assert  (Crest_height == [2.0, 8.0, 3.0, 4.0, 5.0])
+    assert  (ts == [0, 1, 2, 3, 4])
+    assert  (tslosh == 1)
