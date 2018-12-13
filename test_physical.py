a69b6a665c8fbf4bd007d2a95300e099607808be
Boyne272 <boynerichard@yahoo.co.uk>
2018-12-13 19:30:04 +0000

    fixed physical tests

--- test_physical.py
+++ test_physical.py
@@ -8,16 +8,43 @@ Created on Thu Dec 13 16:06:51 2018
 """
 
 import numpy as np
-import sph_stub as sph
 import pandas as pd
+from sph_fe import sph_simulation
+
+
+def test_short_run():
+    """
+    This runs a small simulation to check all the moving parts are working
+    """
+    def f(x, y):
+        if 0 <= y <= 2 or (0 <= x <= 3 and 0 <= y <= 5):
+            return 1
+        else:
+            return 0
+
+    sph_simulation(x_min=[0, 0], x_max=[20, 10], t_final=1, dx=0.8,
+                   func=f, ani=False, file_name="to_travis")
+
 
 def test_speedofsound():
     "test the speed never exceeds speed of sound (20)"
+    # load the data
+    file_name = 'to_travis.csv'
+    data = pd.read_csv(file_name, skiprows=2, index_col=False)
+    data = data.set_index('Time')
+
+    # run the test
     v = np.sqrt(data['V_x']**2+data['V_y']**2)
     assert np.all(v < 20)
 
 
 def test_density():
+    # load the data
+    file_name = 'to_travis.csv'
+    data = pd.read_csv(file_name, skiprows=2, index_col=False)
+    data = data.set_index('Time')
+    times = np.unique(data.index)
+
     dens = data['Density']
     rho0 = dens.loc[0].mean()
 
@@ -39,6 +66,15 @@ def test_density():
 
 
 def test_overlap():
+    """
+    Can only test overlap on a couple of points due to computation time
+    """
+    # load the data
+    file_name = 'to_travis.csv'
+    data = pd.read_csv(file_name, skiprows=2, index_col=False)
+    data = data.set_index('Time')
+    times = np.unique(data.index)
+
     for _ in range(2):
         t = times[np.random.randint(len(times))]
         current = data.loc[t]
