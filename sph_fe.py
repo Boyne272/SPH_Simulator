d9d56ad4bce35245bb23dbdc6341237899fbb5eb
dapolak <deirdree.polak@gmail.com>
2018-12-14 00:19:57 +0000

    added report stuff

--- sph_fe.py
+++ sph_fe.py
@@ -565,6 +565,6 @@ if __name__ == '__main__' and 1:
         else:
             return 0
 
-    sph_simulation(x_min=[0, 0], x_max=[20, 10], t_final=10, dx=0.8, func=f,
-                   path_name='./raw_data/', ani_step=10, ani_key="Pressure",
-                   file_name="first_opt", x_ref=1.9)
+    sph_simulation(x_min=[0, 0], x_max=[20, 10], t_final=30, dx=0.2, func=f,
+                   path_name='./raw_data/', ani = False,
+                   file_name="fe_10_02", x_ref=1.9)
